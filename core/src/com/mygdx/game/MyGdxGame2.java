8b682f3d5f2aa9353d8227d9873004fc2cda8586
TorroisBr <martinshenrique00@gmail.com>
2020-10-20 22:36:31 -0300

    Camera restrição

--- core/src/com/mygdx/game/MyGdxGame2.java
+++ core/src/com/mygdx/game/MyGdxGame2.java
@@ -1,4 +1,5 @@
 package com.mygdx.game;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
@@ -26,8 +27,8 @@ public class MyGdxGame2 extends Game {
     public OrthographicCamera camera;
     public Viewport viewport;
 
-    int recX=50;
-    int recY=50;
+    int recX = 50;
+    int recY = 50;
 
 
     @Override
@@ -41,15 +42,18 @@ public class MyGdxGame2 extends Game {
         renderer = new ShapeRenderer();
 
         camera = new OrthographicCamera();
-        viewport = new FitViewport(telaLarg,telaAlt,camera);
+        viewport = new FitViewport(telaLarg, telaAlt, camera);
         camera.setToOrtho(false, 1280.0F, 4720.0F);
         camera.position.set(1280.0F, 720.0F, 0.0F);
+        camera.position.x = jogador.x + jogador.larg / 2.0F;
+        camera.position.y = jogador.y + jogador.alt / 2.0F;
         camera.update();
     }
+
     @Override
     public void resize(int width, int height) {
         //updated our game viewport
-        viewport.update(width,height);
+        viewport.update(width, height);
 
     }
 
@@ -58,12 +62,11 @@ public class MyGdxGame2 extends Game {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-
+        renderer.setProjectionMatrix(camera.combined);
         batch.setProjectionMatrix(camera.combined);
         Mover();
 
         playerHitbox.setPosition(jogador.x, jogador.y);
-
 
 
         batch.begin();
@@ -75,7 +78,7 @@ public class MyGdxGame2 extends Game {
         renderer.rect(recX, recY, jogador.larg, jogador.alt);
         renderer.end();
 
-        if(playerHitbox.overlaps(doorHitbox)){
+        if (playerHitbox.overlaps(doorHitbox)) {
             System.out.println("AJAIOJIOAJIK");
         }
     }
@@ -92,143 +95,44 @@ public class MyGdxGame2 extends Game {
     }
 
 
-    private void MapaCidadeHitbox() {
-        //Pontos de colisão fim da tela
-        int ladoEsquerdo = 10;
-        int ladoDireito = 1270 - jogador.larg;
-        int ladoTopo = 720 - jogador.alt;
-        int ladoBaixo = 10;
-
-        //Colisões limite da tela
-        if (fundoatual == 2 || fundoatual == 3 || fundoatual == 4 || fundoatual == 8) {
-            if (jogador.x <= ladoEsquerdo) {
-                jogador.x += (jogador.velo);
-
-            }
-        }
-        if (fundoatual == 2 || fundoatual == 3 || fundoatual == 6 || fundoatual == 8) {
-            if (jogador.x >= ladoDireito) {
-                jogador.x += (-jogador.velo);
-            }
-        }
-        if (fundoatual == 2 || fundoatual == 3 || fundoatual == 4) {
-            if (jogador.y >= ladoTopo) {
-                jogador.y += (-jogador.velo);
-            }
-        }
-
-        if (fundoatual == 4 || fundoatual == 6 || fundoatual == 8) {
-            if (jogador.y <= ladoBaixo) {
-                jogador.y += (jogador.velo);
-            }
-        }
-    }
-
     public void MapaCidadeDesenhar() {
-        //Pontos iniciais de transição de tela
-        int xInicial = 40 - (jogador.larg / 2);
-        int xFinal = 1240 - (jogador.larg / 2);
-        int yInicial = 40 - (jogador.alt / 2);
-        int yFinal = 680 - (jogador.alt / 2);
-
-        //Desenhar imagem e transição de tela
-        switch (fundoatual) {
-            case 2:
-
-                if (jogador.y < yInicial) {
-                    fundoatual = 5;
-                    jogador.y = (yFinal);
-                }
-                break;
-            case 3:
-
-                if (jogador.y < yInicial) {
-                    fundoatual = 6;
-                    jogador.y = (yFinal);
-                }
-                break;
-            case 4:
 
 
-                if (jogador.x > xFinal) {
-                    fundoatual = 5;
-                    jogador.x = (xInicial);
-
-                }
-                break;
-            case 5:
-
-                if (jogador.x < xInicial) {
-                    fundoatual = 4;
-                    jogador.x = (xFinal);
-                }
-                if (jogador.y > yFinal) {
-                    fundoatual = 2;
-                    jogador.y = (yInicial);
-                }
-                if (jogador.y < yInicial) {
-                    fundoatual = 8;
-                    jogador.y = yFinal;
-                }
-                if (jogador.x > xFinal) {
-                    fundoatual = 6;
-                    jogador.x = xInicial;
-                }
-                break;
-            case 6:
-
-                if (fundoatual == 6) {
-                    if (jogador.x < xInicial) {
-                        fundoatual = 5;
-                        jogador.x = xFinal;
-                    }
-                    if (jogador.y > yFinal) {
-                        fundoatual = 3;
-                        jogador.y = yInicial;
-                    }
-                }
-                break;
-            case 8:
-
-                if (fundoatual == 8) {
-                    if (jogador.y > yFinal) {
-                        fundoatual = 5;
-                        jogador.y = yInicial;
-                    }
-                }
-                break;
-
-        }
-
-        Q.Desenhar(fundoatual,batch);
+        Q.Desenhar(fundoatual, batch);
 
     }
 
     private void Mover() {
         //Movimento Player-----------------------------------
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
 
-            jogador.x += (-1f * jogador.velo);}
+            jogador.x += (-1f * jogador.velo);
+        }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
 
-            jogador.x += (1f * jogador.velo);}
+            jogador.x += (1f * jogador.velo);
+        }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)){
-            jogador.y += (1f * jogador.velo);}
-
-
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            jogador.y += (-1f * jogador.velo);}
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            jogador.y += (1f * jogador.velo);
+        }
 
 
-        camera.position.x= jogador.x+ jogador.larg/ 2.0F;
-        camera.position.y = jogador.y + jogador.alt / 2.0F;
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            jogador.y += (-1f * jogador.velo);
+        }
+
+        if (jogador.x >= telaLarg / 2 && jogador.x <= telaLarg)
+            camera.position.x = jogador.x;
+
+        //So ta funcionando um
+        if (jogador.y >= telaAlt / 2 && jogador.y <= telaAlt)
+            camera.position.y = jogador.y;
+
 
         camera.update();
     }
-
-
 
 
 }
